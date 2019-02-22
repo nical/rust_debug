be2609ffd5fdbe44c50b5d5ec0c2ee1b2f549355
Nicolas Silva <nical@fastmail.com>
2019-02-22 10:38:56 +0100

    Add serialization and filters.

--- counters/src/lib.rs
+++ counters/src/lib.rs
@@ -1,10 +1,21 @@
+
+#[macro_use]
+#[cfg(feature = "serialization")]
+extern crate serde;
+
+#[cfg(not(feature = "noop"))]
 use std::collections::HashMap;
+#[cfg(not(feature = "noop"))]
 use std::cell::RefCell;
 
+#[derive(Clone, Debug)]
+#[cfg(not(feature = "noop"))]
+#[cfg_attr(feature = "serialization", derive(Serialize, Deserialize))]
 pub struct Counters {
-    events: RefCell<HashMap<&'static str, u64>>
+    events: RefCell<HashMap<String, u64>>
 }
 
+#[cfg(not(feature = "noop"))]
 impl Counters {
     pub fn new() -> Self {
         Counters {
@@ -13,13 +24,18 @@ impl Counters {
     }
 
     /// Increment the counter for the provided event key.
-    pub fn event(&self, key: &'static str) {
-        *self.events.borrow_mut().entry(key).or_insert(0) += 1
+    pub fn event(&self, key: &str) {
+        *self.events.borrow_mut().entry(key.into()).or_insert(0) += 1
     }
 
     /// Reset the counter for the provided event key to zero.
-    pub fn reset(&self, key: &'static str) {
-        self.events.borrow_mut().insert(key, 0);
+    pub fn reset_event(&self, key: &str) {
+        self.events.borrow_mut().insert(key.into(), 0);
+    }
+
+    /// Reset some of the counters to zero.
+    pub fn reset_events<F: Filter>(&self, filter: F) {
+        self.events.borrow_mut().retain(|key, _| !filter.apply(key));
     }
 
     /// Reset all counters to zero.
@@ -27,17 +43,22 @@ impl Counters {
         self.events.borrow_mut().clear();
     }
 
+    /// Keep some of the counters and throw away the rest.
+    pub fn retain<F: Filter>(&self, filter: F) {
+        self.events.borrow_mut().retain(|key, _| filter.apply(key));
+    }
+
     /// Get the value of the counter or zero if it does not exist.
-    pub fn get(&self, key: &'static str) -> u64 {
+    pub fn get(&self, key: &str) -> u64 {
         self.events.borrow().get(key).cloned().unwrap_or(0)
     }
 
     /// Return the sum of all counters with keys containing the provided string.
-    pub fn accumulate(&self, filter: &'static str) -> u64 {
+    pub fn accumulate<F: Filter>(&self, filter: F) -> u64 {
         let mut n = 0;
 
         for (key, value) in self.events.borrow().iter() {
-            if key.contains(filter) {
+            if filter.apply(key) {
                 n += value
             }
         }
@@ -48,16 +69,75 @@ impl Counters {
     /// Print the counters to stdout.
     ///
     /// If a string is passed, only counters with keys containing that string will be printed.
-    pub fn print(&self, filter: Option<&'static str>) {
+    pub fn print_to_stdout<F: Filter>(&self, filter: F) {
         for (key, value) in self.events.borrow().iter() {
-            if filter.map(|s| key.contains(s)).unwrap_or(true) {
+            if filter.apply(key) {
                 println!("{}: {}", key, value);
             }
         }
     }
+
+    /// Moves all the elements of `other` into `self`, leaving `other` empty.
+    pub fn append(&self, other: &Counters) {
+        for (key, value) in other.events.borrow_mut().drain() {
+            *self.events.borrow_mut().entry(key).or_insert(0) += value;
+        }
+    } 
+}
+
+pub trait Filter {
+    fn apply(&self, key: &str) -> bool;
+}
+
+impl<'l> Filter for &'l str {
+    fn apply(&self, key: &str) -> bool {
+        key.contains(self)
+    }
+}
+
+impl Filter for Fn(&str) -> bool {
+    fn apply(&self, key: &str) -> bool {
+        self(key)
+    }
+}
+
+pub struct All;
+impl Filter for All {
+    fn apply(&self, _: &str) -> bool { true }
+}
+
+pub struct StartsWith<'l>(pub &'l str);
+impl<'l> Filter for StartsWith<'l> {
+    fn apply(&self, key: &str) -> bool {
+        key.starts_with(self.0)
+    }
+}
+
+pub struct EndsWith<'l>(pub &'l str);
+impl<'l> Filter for EndsWith<'l> {
+    fn apply(&self, key: &str) -> bool {
+        key.ends_with(self.0)
+    }
+}
+
+#[cfg(feature = "noop")]
+pub struct Counters;
+
+#[cfg(feature = "noop")]
+impl Counters {
+    pub fn new() -> Self { Counters }
+    pub fn event(&self, _key: &str) {}
+    pub fn reset_event(&self, _key: &str) {}
+    pub fn reset_events<F: Filter>(&self, _filter: F) {}
+    pub fn reset_all(&self) {}
+    pub fn retain<F: Filter>(&self, _filter: F) {}
+    pub fn get(&self, _key: &str) -> u64 { 0 }
+    pub fn accumulate<F: Filter>(&self, _filter: F) -> u64 { 0 }
+    pub fn print_to_stdout<F: Filter>(&self, _filter: F) {}
 }
 
 #[test]
+#[cfg(not(feature = "noop"))]
 fn it_works() {
     let counters = Counters::new();
 
@@ -72,6 +152,42 @@ fn it_works() {
     assert_eq!(counters.get("foo::bar"), 2);
     assert_eq!(counters.get("foo::baz"), 1);
     assert_eq!(counters.accumulate("foo::"), 3);
+
+    counters.reset_events(EndsWith("bar"));
+
+    assert_eq!(counters.get("foo::bar"), 0);
+    assert_eq!(counters.get("foo::baz"), 1);
+    assert_eq!(counters.accumulate("foo::"), 1);
+
+    counters.reset_all();
+
+    assert_eq!(counters.get("foo::bar"), 0);
+    assert_eq!(counters.get("foo::baz"), 0);
+    assert_eq!(counters.accumulate("foo::"), 0);
+}
+
+#[test]
+#[cfg(feature = "noop")]
+fn noop() {
+    let counters = Counters::new();
+
+    counters.event("foo::bar");
+    counters.event("foo::bar");
+
+    counters.event("foo::baz");
+
+    counters.event("meh");
+    counters.event("fooo");
+
+    assert_eq!(counters.get("foo::bar"), 0);
+    assert_eq!(counters.get("foo::baz"), 0);
+    assert_eq!(counters.accumulate("foo::"), 0);
+
+    counters.reset_events(EndsWith("bar"));
+
+    assert_eq!(counters.get("foo::bar"), 0);
+    assert_eq!(counters.get("foo::baz"), 0);
+    assert_eq!(counters.accumulate("foo::"), 0);
 
     counters.reset_all();
 
